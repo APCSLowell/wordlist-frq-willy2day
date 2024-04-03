7ba6a93d5e27fb591a1121d10ec6d90e9b58ec5e
willy2day <143228693+willy2day@users.noreply.github.com>
2024-04-03 12:00:12 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,22 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int k=0;
+    for(int i=0; i<myList.size();i++){
+      if(myList.get(i).length()==len)
+        k++;
+    }
+    return k;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+  for(int i=0; i<myList.size();i++){
+    if(myList.get(i).length()==len){
+      myList.remove(i);
+      i--;
+    }
+  }
   }
 }
